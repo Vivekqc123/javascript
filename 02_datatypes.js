224ec8743cbbc1cd471be70c5cef606d85f7232a
Vivek Gupta <98093405+Vivekqc123@users.noreply.github.com>
2025-05-31 20:42:21 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3+3)// we are using nodejs not browser
+
+console.log(3
+    +3
+  ); // there should be code readability
+
+  let name ="hitesh"
+  let age = 19
+  let isLoggedIn = false 
+
+  // number => 2 to powe 53
+  // bigInt
+  // string => ""
+  // boolean => true/false
+  // null => standalone value
+  // undefined => when you don't ASSIGN value to a variable
+  let state = null // yaha statr ki value empty hai
+
+  // symbol => unique
+
+
+  // object
+
+  console.log(typeof age);
+  
+
+
+
+
+
---
+++ 03_conversionOperation.js
@@ -0,0 +1,21 @@
+/* let score="null"
+
+console.log(typeof score);
+console.log(typeof(score)); */
+
+let valueInNumber = Number(score) // conversion of string to a number or anything to anumber
+let abc= String(score) // converstion of string to a string or anything to a string 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // get NaN  
+
+// "33" => 33
+// "33abc" => Nan ( type lkin number hi hai)
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+ 
+
